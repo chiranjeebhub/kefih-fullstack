4f1baa1f22c0aac894f37398cd1a9314c3c5ad19
Siva <sivaramakannan05@gmail.com>
2023-10-08 22:24:07 +0530

    OTP issue fixed

--- app/Helpers/CommonHelper.php
+++ app/Helpers/CommonHelper.php
@@ -2579,6 +2579,8 @@ $api_key = '45EDF740109A10';
                     curl_setopt($ch, CURLOPT_POST, true);
                     curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
                     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+                    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);        
                     $response = curl_exec($ch);
                     curl_close($ch);
                     
